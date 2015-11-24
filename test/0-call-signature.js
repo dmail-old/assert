1916cf65909a894d7c4226aa5f1dec998dae5d64
Damien Maillard <dmaillard06@gmail.com>
2015-11-24 15:17:51 +0100

    callSignature tests are done

--- test/0-call-signature.js
+++ test/0-call-signature.js
@@ -1,12 +1,70 @@
 import CallSignature from '../lib/api/call-signature.js';
 import assert from 'node/assert';
 
-var emptySignature = CallSignature.create();
+function signatureEquals(callSignature, args, expectedSignedArgs){
+	assert.deepEqual(callSignature.signArguments(args), expectedSignedArgs);
+}
 
-assert.equal(emptySignature.testArguments([]), true);
-assert.equal(emptySignature.testArguments([0]), false);
+function signaturePass(callSignature, args){
+	signatureEquals(callSignature, args, args);
+}
 
-var oneArgSignature = CallSignature.create(undefined);
+function signatureFail(callSignature, args){
+	assert.throws(function(){
+		callSignature.signArguments(args);
+	}, function(e){
+		return e.name == 'SignatureError';
+	});
+}
 
-assert.equal(oneArgSignature.testArguments([]), false);
-assert.equal(oneArgSignature.testArguments([0]), true);
+export default function(){
+	var allowAnyArgSignature = CallSignature.create();
+
+	signaturePass(allowAnyArgSignature, []);
+	signaturePass(allowAnyArgSignature, ['a', 10]);
+
+	var allowExactlyOneArgSignature = CallSignature.create(undefined);
+
+	signaturePass(allowExactlyOneArgSignature, [undefined]);
+	signaturePass(allowExactlyOneArgSignature, [10]);
+	signatureFail(allowExactlyOneArgSignature, []);
+
+	var allowAtLeastOneArgSignature = CallSignature.create(undefined, '...');
+
+	signaturePass(allowAtLeastOneArgSignature, [undefined]);
+	signaturePass(allowAtLeastOneArgSignature, [10, 11]);
+	signatureFail(allowAtLeastOneArgSignature, []);
+
+	var expectOneStringSignature = CallSignature.create({
+		type: String
+	});
+
+	signaturePass(expectOneStringSignature, ['']);
+	// jshint ignore: start
+	signaturePass(expectOneStringSignature, [new String('')]);
+	// jshint ignore: end
+	signatureFail(expectOneStringSignature, [10]);
+
+	var expectOneStringProvidingDefaultValidValue = CallSignature.create({
+		type: String,
+		value: 'foo'
+	});
+
+	signatureEquals(expectOneStringProvidingDefaultValidValue, ['hello'], ['hello']);
+	signatureEquals(expectOneStringProvidingDefaultValidValue, [], ['foo']);
+
+	var expectTwoArgWithSecondDependingOnFirst = CallSignature.create(
+		undefined, {value(a){ return a + 1; }}
+	);
+
+	signatureEquals(expectTwoArgWithSecondDependingOnFirst, [1], [1, 2]);
+	signatureEquals(expectTwoArgWithSecondDependingOnFirst, [1, 3], [1, 3]);
+
+	var expectThisToBeAString = CallSignature.create();
+	expectThisToBeAString.expectedThis = String;
+	expectThisToBeAString.thisValue = 'foo';
+
+	assert.equal(expectThisToBeAString.signThis(''), '');
+	assert.equal(expectThisToBeAString.signThis(undefined), 'foo');
+}
+
--- test/index.js
+++ test/index.js
@@ -1,6 +1,40 @@
-import "./0-call-signature.js";
-import "./0-api.js";
-import "./1-assertion.js";
-import "./2-core.js";
+import nodeFs from 'node/fs';
 
-console.log('all test passed');
+var testDirectoryLocation = platform.locate('./test', true);
+
+var testFileLocations = nodeFs.readdirSync(testDirectoryLocation).filter(function(fileName){
+	return fileName != 'index.js';
+}).map(function(fileName){
+	return 'file:///' + testDirectoryLocation + '/' + fileName;
+});
+
+var testPromise = testFileLocations.reduce(function(previous, current){
+	return previous.then(function(){
+		return System.import(current).then(function(exports){
+			return exports['default'];
+		}).then(function(test){
+			if( test ){
+				if( typeof test != 'function' ){
+					throw new Error(current + ' export default must be a function');
+				}
+				console.log(current);
+
+				return new Promise(function(resolve){
+					resolve(test());
+				}).then(function(){
+					console.log('\tpassed');
+				}, function(error){
+					console.log('\tfailed', error);
+					return Promise.reject(error);
+				});
+			}
+			return undefined;
+		});
+	});
+}, Promise.resolve());
+
+testPromise.then(function(){
+	console.log('Finished');
+}, function(error){
+	console.error(error.stack);
+});
